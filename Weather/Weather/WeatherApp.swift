f320bda4778ab209c9f1bb7b117474a96458a8b3
Stephan Howey <stephan.howey@icloud.com>
2023-06-10 17:15:38 +0200

    initial project

---
+++ Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Stephan Howey on 10.06.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
